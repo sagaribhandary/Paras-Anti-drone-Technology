860d9f955f117f8a0e420e53003ce8c62fcb58d4
sagaribhandary <sagariswam@gmail.com>
2024-11-30 17:38:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('https://github.com/sagaribhandary/Paras-Anti-drone-Technology/blob/main/service_worker.js ')
-    .then(function(registration) {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(function(error) {
-      console.log('Service Worker registration failed:', error);
-    });
-}
+if ('serviceWorker' in navigator) 
+navigator.serviceWorker.register('https://sagaribhandary.github.io/Paras-Anti-drone-Technology/service_worker.js')
+  .then(function(registration) {
+    console.log('Service Worker registered with scope:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service Worker registration failed:', error);
+  });
